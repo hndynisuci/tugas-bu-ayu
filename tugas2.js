96ca681d2b7ea325c5b3fe32ffce7f0b11b8fc17
hndynisuci <hndynisuci4@gmail.com>
2025-01-14 21:29:07 -0800

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+let namaDepan ="Suci"
+let namaBelakang ="Handayani"
+
+let nama ="Suci Handayani";
+let alamat ="waru mranggen demak";
+let usia ="15";
+let ttl ="demak, 16 februari 2009";
+let gender ="perempuan";
+let agama ="islam";
+let email ="hndynisuci@gmail.com";
+let berat ="40";
+let tinggi ="148";
+let pekerjaaan ="pelajar";
+let sekolah ="SMKN 1 SAYUNG"
+let jurusan "X PPLG 1";
+
+document.write =("nama saya adalah "+nama Depan."."+nama Tengah""+namaBelakang + " " + "saya tinggal
